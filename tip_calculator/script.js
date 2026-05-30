991d3de6cdb5acd80e3cce7b0edb02fb56233e25
Prabesh <tandukarprabesh@gmail.com>
2026-05-30 15:22:56 +1200

    tip calculator

--- tip_calculator/script.js
+++ tip_calculator/script.js
@@ -1,41 +1,41 @@
-const ten_percent_btn = document.getElementById("ten_percent");
-const fifteen_percent_btn = document.getElementById("fifteen_percent");
-const twenty_percent_btn = document.getElementById("twenty_percent");
-const twenty_five_percent_btn = document.getElementById("twenty_five_percent");
 let tip;
+const tip_btn = document.querySelectorAll(".tip_btn");
+const bill_value = document.getElementById("total");
+const people_num = document.getElementById("people");
+const tip_person_dis = document.querySelector(".tip_per_person");
+const total_person_dis = document.querySelector(".total_per_person");
+const total_with_tips = document.querySelector(".total_with_tips");
+const calculate_btn = document.querySelector(".calculate");
 
-ten_percent_btn.addEventListener("click", function () {
-  ten_percent_btn.classList.add("active");
-  fifteen_percent_btn.classList.remove("active");
-  twenty_percent_btn.classList.remove("active");
-  twenty_five_percent_btn.classList.remove("active");
-  tip = 10;
-  console.log(tip);
+tip_btn.forEach((element) => {
+  element.addEventListener("click", () => {
+    tip_btn.forEach((ele) => {
+      ele.classList.remove("active");
+    });
+    element.classList.add("active");
+    tip = Number(element.dataset.tip);
+  });
 });
 
-fifteen_percent_btn.addEventListener("click", function () {
-  ten_percent_btn.classList.remove("active");
-  fifteen_percent_btn.classList.add("active");
-  twenty_percent_btn.classList.remove("active");
-  twenty_five_percent_btn.classList.remove("active");
-  tip = 15;
-  console.log(tip);
-});
+function calculate() {
+  const bill_amount = Number(bill_value.value);
+  const people = Number(people_num.value);
 
-twenty_percent_btn.addEventListener("click", function () {
-  ten_percent_btn.classList.remove("active");
-  fifteen_percent_btn.classList.remove("active");
-  twenty_percent_btn.classList.add("active");
-  twenty_five_percent_btn.classList.remove("active");
-  tip = 20;
-  console.log(tip);
-});
+  if (!bill_amount) {
+    return;
+  } else if (!people) {
+    return;
+  } else if (!tip) {
+    return;
+  } else {
+    const tip_amount = bill_amount * (tip / 100);
+    const total_amount = bill_amount + tip_amount;
+    const tip_per_person = tip_amount / people;
+    const total_per_person = total_amount / people;
+    total_with_tips.textContent = `Total amount with tips: ${total_amount.toFixed(2)}`;
+    tip_person_dis.textContent = `Tips per person: ${tip_per_person.toFixed(2)}`;
+    total_person_dis.textContent = `Total per person: ${total_per_person.toFixed(2)}`;
+  }
+}
 
-twenty_five_percent_btn.addEventListener("click", function () {
-  ten_percent_btn.classList.remove("active");
-  fifteen_percent_btn.classList.remove("active");
-  twenty_percent_btn.classList.remove("active");
-  twenty_five_percent_btn.classList.add("active");
-  tip = 25;
-  console.log(tip);
-});
+calculate_btn.addEventListener("click", calculate);
