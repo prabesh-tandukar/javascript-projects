51611e087cf19b98e48c3bd06831d6971edfe705
Prabesh <tandukarprabesh@gmail.com>
2026-08-31 22:36:33 +1200

    ai workout generator

---
+++ ai-workout-coach/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+function App() {
+  const [goal, setGoal] = useState("");
+  const [time, setTime] = useState();
+  const [equipment, setEquipment] = useState("");
+  const [experience, setExperience] = useState("beginner");
+
+  function generateWorkout() {
+    console.log(goal, time, equipment, experience);
+  }
+  return (
+    <div>
+      <h1>AI Workout Coach</h1>
+
+      <div className="field">
+        <label>Goal</label>
+        <input
+          value={goal}
+          onChange={(e) => setGoal(e.target.value)}
+          placeholder="eg. build muscle"
+        />
+      </div>
+      <div className="field">
+        <label>Time Available</label>
+        <input
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+          placeholder="Enter in minutes"
+        />
+      </div>
+      <div className="field">
+        <label>Equipment</label>
+        <input
+          value={equipment}
+          onChange={(e) => setEquipment(e.target.value)}
+          placeholder="What equipments do you have ?"
+        ></input>
+      </div>
+      <div className="field">
+        <label>Select you experience level:</label>
+        <select
+          value={experience}
+          onChange={(e) => setExperience(e.target.value)}
+        >
+          <option value="beginner">Beginner</option>
+          <option value="intermediate">Intermediate</option>
+          <option value="advanced">Advanced</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={generateWorkout}>Generate Workout</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
